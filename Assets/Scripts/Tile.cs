5555a27caaa6fefe7e455691417677857267ac72
Santo Commarato <cylynex@gmail.com>
2018-04-02 10:14:39 -0600

    setup

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Tile {
+
+    public enum TileType { Empty, Floor };
+    public TileType type = TileType.Empty;
+
+    Action cbTileTypeChanged;
+
+    LooseObject looseObject;
+    InstalledObject installedObject;
+
+    World world;
+    public int x;
+    public int y;
+
+    public Tile(World world, int x, int y) {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    World world;
+
+    public Sprite floorSprite;
+
+	// Use this for initialization
+	void Start () {
+        world = new World();
+        world.RandomizeTiles();
+
+        // create gameobject for each tile
+        for (int x = 0; x < world.width; x++) {
+            for (int y = 0; y < world.height; y++) {
+                // create tile now
+                GameObject tileObject = new GameObject();
+                tileObject.name = "Tile_" + x + "_" + y;
+                SpriteRenderer tileSprite = tileObject.AddComponent<SpriteRenderer>();
+                Tile tileData = world.GetTileAt(x, y);
+                if (tileData.type == Tile.TileType.Floor) {
+                    Debug.Log("floor");
+                    tileSprite.sprite = floorSprite;
+                } else if (tileData.type == Tile.TileType.Empty) {
+                    Debug.Log("empty");
+                }
+
+                tileObject.transform.position = new Vector2(tileData.x, tileData.y);
+
+            }
+        }
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    void OnTileTypeChanged(Tile tileData, GameObject tileObject) {
+        if (tileData.type == Tile.TileType.Floor) {
+            tileObject.GetComponent<SpriteRender>().sprite = floorSprite;
+        } else if (tileData.type == Tile.TileType.Empty) {
+            tileObject.GetComponent<SpriteRenderer>().sprite = null;
+        } else {
+            Debug.LogError("Changing File Type - Unrecognized tile");
+        }
+    }
+
+}
